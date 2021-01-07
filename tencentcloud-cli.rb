fca28afd6be01155186ff26ed97dc8271140a1d4
ethanswang <ethanswang@tencent.com>
2021-01-07 15:10:11 +0800

    homebrew-tccli

---
+++ tencentcloud-cli.rb
@@ -0,0 +1,44 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TencentcloudCli < Formula
+  include Language::Python::Virtualenv
+  desc "Tencent Cloud API 3.0 Command Line Interface"
+  homepage "https://github.com/TencentCloud/tencentcloud-cli"
+  url "https://github.com/TencentCloud/tencentcloud-cli/archive/3.0.319.1.tar.gz"
+  sha256 "9adff8feb7e40d1327ec37da0a0ab7b41773c3bac09d1f7fdba329ba3cf5e756"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+  depends_on "python@3.9"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", 
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "tccli"
+    venv.pip_install_and_link buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "pyinstaller"
+
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tencentcloud-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
