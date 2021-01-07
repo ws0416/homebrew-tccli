67c426a7cba5e539528aba8891d1fa2b30e35faf
ws0416 <wsng0416@163.com>
2021-01-07 19:29:23 +0800

    update version

--- tccli.rb
+++ tccli.rb
@@ -4,12 +4,11 @@
 class Tccli < Formula
   include Language::Python::Virtualenv
   desc "Tencent Cloud API 3.0 Command Line Interface"
-  homepage "https://github.com/TencentCloud/tencentcloud-cli"
-  url "https://github.com/TencentCloud/tencentcloud-cli/archive/3.0.319.1.tar.gz"
+  homepage "https://cloud.tencent.com/document/product/440/6176"
+  url "https://github.com/TencentCloud/tencentcloud-cli/archive/3.0.320.1.tar.gz"
   sha256 "9adff8feb7e40d1327ec37da0a0ab7b41773c3bac09d1f7fdba329ba3cf5e756"
   license "Apache-2.0"
 
-  # depends_on "cmake" => :build
   depends_on "python@3.9"
 
   def install
@@ -20,13 +19,6 @@ class Tccli < Formula
     venv.pip_install_and_link buildpath
     system libexec/"bin/pip", "uninstall", "-y", "pyinstaller"
 
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
   end
 
   test do
